a3b38da50e701f0166a0fde0592654ea9e3697af
marinabarrios <marina_paolab@hotmail.com>
2024-08-11 23:34:12 -0300

    cambio la manera de generar los usuarios

--- ComfyChair.js
+++ ComfyChair.js
@@ -1,22 +1,47 @@
+const Autores = require('./Autores.js');
+const Chairs = require('./Chairs.js');
 const Conferencias = require('./Conferencias.js');
-const Usuarios = require('./Usuarios.js');
+const Revisores = require('./Revisores.js');
 
 class ComfyChair {
     constructor() {
         this._conferencias = [];
         this._usuarios = [];
+        this._chairs = [];
+        this._autores = [];
+        this._revisores = [];
     }
 
-    registrarUsuario(nombreUsuario, afiliacion, email, contrasenia) {
+    registrarUsuario(tipo, nombreUsuario, afiliacion, email, contrasenia) {
         //verifico que no exista ya//
-        if(this._usuarios.some((user) => user.nombreUsuario() === nombreUsuario)){
+        if(this._usuarios.some((user) => user.nombreUsuario() === nombreUsuario)){// la clase nombreUsuario() NO puedo instanciar
             //Comente la siguiente linea para poder usarlo en el test//
             //throw new Error('Ya existe un usuario con ese nombre');
             console.log('Ya existe un usuario con ese nombre');
         }
         
         //luego lo guardo en una colección//
-        const user = new Usuarios(nombreUsuario, afiliacion, email, contrasenia);
+       /* const user = new Usuarios(nombreUsuario, afiliacion, email, contrasenia);        
+        this._usuarios.push(user);
+        return user;*/
+
+        let user;
+        switch(tipo) {
+            case 'chair':
+                user = new Chairs(nombreUsuario, afiliacion, email, contrasenia);
+                this._chairs.push(user);
+                break;
+            case 'revisor':
+                user = new Revisores(nombreUsuario, afiliacion, email, contrasenia);
+                this._revisores.push(user);
+                break;
+            case 'autor':
+                user = new Autores(nombreUsuario, afiliacion, email, contrasenia);
+                this._autores.push(user);
+                break;
+            default:
+                throw new Error("Tipo de usuario no válido.");
+        }
         this._usuarios.push(user);
         return user;
     }
@@ -25,14 +50,25 @@ class ComfyChair {
         return this._usuarios;
     }
 
-    crearConferencia(nombreConferencia, fechaInicio, fechaFin) {
-        const nuevaConferencia = new Conferencias (nombreConferencia,fechaInicio, fechaFin);
+    listChairs() {
+        return this._chairs;
+    }
+
+    listRevisores() {
+        return this._revisores;
+    }
+
+    listAutores() {
+        return this._autores;
+    }
+
+    crearConferencia(nombreConferencia, fechaInicio, fechaFin, organizadores, comite, autores) {
+        const nuevaConferencia = new Conferencias (nombreConferencia,fechaInicio, fechaFin, organizadores, comite, autores);
         this._conferencias.push(nuevaConferencia);
         return nuevaConferencia;
     }    
 
     listConferencias() {
-        console.log(this._conferencias);
         return this._conferencias;            
     }    
 }
