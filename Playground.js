b690c04cdc3df4fbbd8184674906a5005c6bcdb7
marinabarrios <marina_paolab@hotmail.com>
2024-08-11 23:37:01 -0300

    voy completando el playground

--- Playground.js
+++ Playground.js
@@ -1,48 +1,56 @@
 const ComfyChair = require('./ComfyChair.js');
-const Conferencias = require('./Conferencias.js');
-//const Usuarios = require('./Usuarios.js');
 
 const empresa = new ComfyChair();
 /* REGISTRO USUARIOS */
-const juan = empresa.registrarUsuario('Juan Rodriguez', 'UNLP', 'juan_rodriguez@gmail.com', '123456');
-const jose = empresa.registrarUsuario('José Gonzalez', 'UNNE', 'jose_gonzalez@gmail.com', '123456');
-const matias = empresa.registrarUsuario('Matias Lei', 'UNAM', 'matias_lei@gmail.com', '123456');
-const leo = empresa.registrarUsuario('Leonardo Rey', 'UNAM', 'leonardo_rey@gmail.com', '123456');
-const maria = empresa.registrarUsuario('Maria Gonzalez', 'UNNE', 'maria_gonzalez@gmail.com', '123456');
-const juana = empresa.registrarUsuario('Juana Gómez', 'UNLP', 'juana_gomez@gmail.com', '123456');
-//const juanr = empresa.registrarUsuario('Juan Rodriguez', 'UNLP', 'juan_rodriguez@gmail.com', '123456');
+const juan = empresa.registrarUsuario('chair', 'Juan Rodriguez', 'UNLP', 'juan_rodriguez@gmail.com', '123456');
+const jose = empresa.registrarUsuario('autor','José Gonzalez', 'UNNE', 'jose_gonzalez@gmail.com', '123456');
+const matias = empresa.registrarUsuario('autor','Matias Lei', 'UNAM', 'matias_lei@gmail.com', '123456');
+const leo = empresa.registrarUsuario('autor','Leonardo Rey', 'UNAM', 'leonardo_rey@gmail.com', '123456');
+const maria = empresa.registrarUsuario('revisor','Maria Gonzalez', 'UNNE', 'maria_gonzalez@gmail.com', '123456');
+const juana = empresa.registrarUsuario('revisor','Juana Gómez', 'UNLP', 'juana_gomez@gmail.com', '123456');
+//const juanr = empresa.registrarUsuario('revisor','Juan Rodriguez', 'UNLP', 'juan_rodriguez@gmail.com', '123456');
 const todosLosUsuarios = empresa.listUsuarios();
-console.log('todosLosUsuarios',todosLosUsuarios);
+const todosLosChairs = empresa.listChairs();
+const todosLosRevisores = empresa.listRevisores();
+const todosLosAutores = empresa.listAutores();
+//console.log('todosLosUsuarios',empresa.listUsuarios());
+//console.log('todosLosChairs',empresa.listChairs());
+//console.log('todosLosRevisores',empresa.listRevisores());
+//console.log('todosLosAutores',empresa.listAutores());
 
 /** CREO CONFERENCIAS */
-const nuevaConferencia = empresa.crearConferencia('Conferencia Informática', 
-                                                  '2024-07-28', 
-                                                  '2024-07-31', 
-                                                  empresa);
-const otraConferencia = empresa.crearConferencia('Conferencia Robótica', 
-                                                    '2024-08-28', 
-                                                    '2024-08-31', 
-                                                  empresa);
+const conferenciaInformatica = empresa.crearConferencia('Conferencia Informática', '2024-07-28', '2024-07-31', 
+                                                  todosLosChairs, todosLosRevisores, todosLosAutores);
+const conferenciaRobotica = empresa.crearConferencia('Conferencia Robótica', '2024-08-28', '2024-08-31',
+                                                 todosLosChairs, todosLosRevisores, todosLosAutores);
 
-console.log('DatosNuevaConferencia',nuevaConferencia);
-/** DEFINO LOS ROLES DE LOS USUARIOS PARA UNA CONFERENCIA */
-const conferencia = new Conferencias();
-const conferenciaInformatica = conferencia.definirRoles(nuevaConferencia);
-console.log(conferenciaInformatica);
-/*const todasLasConferencias = empresa.listConferencias();console.log('todasLasConferencias ',todasLasConferencias);
-//const conferenciaExistente = todasLasConferencias.some((conferencia) => conferencia._nombreConferencia === 'Conferencia Informática');       
-const nombreABuscar = 'Conferencia Informática';
+//console.log('DatosConferenciaInformatica',conferenciaInformatica);
 
-const conferenciaExistente = todasLasConferencias.find(conferencia => conferencia._nombreConferencia === nombreABuscar);
-console.log('ExisteConferencia',conferenciaExistente)
-if (conferenciaExistente) {
-    conferenciaExistente._organizadores = organizadores;
-    conferenciaExistente._comite = comite;
-    const indiceConferencia = todasLasConferencias.indexOf(conferenciaExistente);
-    todasLasConferencias[indiceConferencia] = conferenciaExistente;
-    console.log('INDEX', todasLasConferencias[indiceConferencia]);
-    console.log('La conferencia existe. Actualizando roles.');
-} else {
-    console.log('La conferencia no existe.');
-}*/
+/** CREO LA SESION */
+const sesionInteligencia = conferenciaInformatica.crearSesion('Inteligencia Artificial', 'regular',
+                                                              '2024-08-15', 'recepcion');
+const sesionInfraestructura = conferenciaInformatica.crearSesion('Infraestructura', 'workshop',
+                                                                 '2024-08-15', 'recepcion');
+const sesionSeguridad = conferenciaInformatica.crearSesion('Seguridad Infórmatica', 'poster',
+                                                           '2024-08-15', 'recepcion');
+//console.log('todasLasSesiones',conferenciaInformatica.listSesiones());
+
+/** UN AUTOR CREA UN ARTICULO */
+const futureOfProjectManagement = jose.crearArticulo(1, 'An investigation into the Impact of Artificial Intelligence on the Future of Project Management',
+                                                     'regular', 'The purpose of the study is to investigate the impact of Artificial Intelligence',// on the future of Project Management. This study provides detailed conceptual information about Artificial Intelligence and different perspectives. Artificial Intelligence is defined as the new technical discipline, which would develop an application system, a technological method in order to simulate the expansion and extension of human intelligence.',
+                                                     'https://ieeexplore.ieee.org/document/9430234',
+                                                     [jose, matias], null,
+                                                     matias, new Date()
+                                                    );
+//console.log('listArticulosCreados',jose.listArticulosCreados());
+
+/** EL AUTOR ENVIA EL ARTICULO */
+const envioFutureOfProjectManagement = jose.enviarArticulo(sesionInteligencia, futureOfProjectManagement);
+
+/** LA SESION RECIBE EL ARTICULO */
+const algo = sesionInteligencia.recibirArticulo(futureOfProjectManagement);
+console.log ('Se recibió el artículo',algo);
+/** SE VERIFICA SI EL AUTOR TIENE NOTIFICACIONES DE RECHAZADO SOBRE SU ARTICULO */
+console.log(matias.obtenerNotificaciones());
+
 empresa;
